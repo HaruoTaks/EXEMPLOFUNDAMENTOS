188e3aa7aaf50cf165740c186b9018b29d26726d
HaruoTaks <haruo.takahashi@outlook.com>
2025-07-14 20:27:20 -0300

    Atualização

--- ExemploFundamentos.Common/obj/Debug/net7.0/ExemploFundamentos.Common.AssemblyInfo.cs
+++ ExemploFundamentos.Common/obj/Debug/net7.0/ExemploFundamentos.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploFundamentos.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20866759ca353da67d31640c014ac3d243427af7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploFundamentos.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploFundamentos.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ExemploFundamentos.NET6/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿Console.WriteLine("Hello World com método main no NET6");
- 
